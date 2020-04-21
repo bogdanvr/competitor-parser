8cd287f72033f4fbfbbb095df5d6446e3678581e
bogdanvr <provisionim@gmail.com>
2020-04-21 10:55:52 +0600

    reformat file

--- bs-el.py
+++ bs-el.py
@@ -8,8 +8,6 @@ We get all categories of goods, then to go through them with a file "bs-el-pr.py
 
 """
 
-
-
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen
 from urllib.error import HTTPError
@@ -17,54 +15,44 @@ import re
 import ssl
 from datetime import datetime
 
-
 now = datetime.now()
 date = str(now).split(' ')[0]
-
-
-
-
-
 
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
-
-
-
 
 text = 0
 pages = set()
 h1 = set()
 promo = set()
 
+
 def get_page(urlcomp):
     global pages
     global text
     global h1
     global promo
-    
-    #try to get html
+
+    # try to get html
     try:
         html = urlopen(urlcomp, context=ctx)
-        
-    
-        
     except HTTPError:
         return print('no html')
+
     try:
         bsobj = bs(html, 'html.parser')
     except:
         print("no bsobj")
-    
-    #try to get page title
+
+    # try to get page title
     try:
         head = bsobj.h1.get_text()
         h1.add(head)
         print(head)
     except AttributeError:
         print('No header')
-        
+
     # try to get page description
     try:
         if bsobj.find('div', {'class', 'bxr-section-desc'}):
@@ -75,25 +63,21 @@ def get_page(urlcomp):
             print('All pages-{} No text'.format(len(pages)))
     except AttributeError:
         print('No text in this page')
-    
+
     # try to get URL
-    for i in bsobj.find_all('a', href=re.compile("(/catalog/)") ):
+    for i in bsobj.find_all('a', href=re.compile("(/catalog/)")):
         if 'href' in i.attrs:
             if i.attrs['href'] not in pages:
-                
-                
-                
+
                 if len(i.attrs['href'].split('/')) < 7:
                     print(len(i.attrs['href'].split('/')))
-                    
-                    
+
                     if '?' not in i.attrs['href']:
-                                        
-                        
                         newpage = i.attrs['href']
                         print(newpage)
                         pages.add(newpage)
                         get_page(newpage)
+
 
 get_page("https://elomsk.ru")
 
